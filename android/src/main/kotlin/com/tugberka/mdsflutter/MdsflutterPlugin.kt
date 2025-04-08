89d0e51495826435fab8cce418ede7b720782885
Panagiotis Giannoutsos <36935711+Panosfunk@users.noreply.github.com>
2025-04-08 06:12:53 +0200

    Removing old package (#33)
    * removing PluginRegistry.Registrar
    
    * bumping version and changelog
    
    ---------
    
    Co-authored-by: petri-lipponen-movesense <95341815+petri-lipponen-movesense@users.noreply.github.com>

--- android/src/main/kotlin/com/tugberka/mdsflutter/MdsflutterPlugin.kt
+++ android/src/main/kotlin/com/tugberka/mdsflutter/MdsflutterPlugin.kt
@@ -16,6 +16,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+
 import java.util.*
 
 /** MdsflutterPlugin */
