bf99826953e8baf2882fc4941547f37990ed3c9c
petri-lipponen-movesense <95341815+petri-lipponen-movesense@users.noreply.github.com>
2026-08-17 09:23:14 +0300

    Technical: protobuf update (#45)
    * Technical: protobuf update
    - update protobuf to 6.0.0
    - update android sdk 36
    - update gradle plugin
    
    * - change version to 2.4.0-alpha1
    - allow >= for protobuf version
    
    * - fix CHANGELOG.md
    - limit protobuf to <7

--- lib/gen/protos/mdsflutter.pbjson.dart
+++ lib/gen/protos/mdsflutter.pbjson.dart
@@ -1,57 +1,76 @@
-///
-//  Generated code. Do not modify.
-//  source: mdsflutter.proto
+// This is a generated file - do not edit.
 //
-// @dart = 2.12
-// ignore_for_file: annotate_overrides,camel_case_types,constant_identifier_names,deprecated_member_use_from_same_package,directives_ordering,library_prefixes,non_constant_identifier_names,prefer_final_fields,return_of_invalid_type,unnecessary_const,unnecessary_import,unnecessary_this,unused_import,unused_shown_name
+// Generated from protos/mdsflutter.proto.
 
-import 'dart:core' as $core;
+// @dart = 3.3
+
+// ignore_for_file: annotate_overrides, camel_case_types, comment_references
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: curly_braces_in_flow_control_structures
+// ignore_for_file: deprecated_member_use_from_same_package, library_prefixes
+// ignore_for_file: non_constant_identifier_names, prefer_relative_imports
+// ignore_for_file: unused_import
+
 import 'dart:convert' as $convert;
+import 'dart:core' as $core;
 import 'dart:typed_data' as $typed_data;
+
 @$core.Deprecated('Use requestResultDescriptor instead')
-const RequestResult$json = const {
+const RequestResult$json = {
   '1': 'RequestResult',
-  '2': const [
-    const {'1': 'requestId', '3': 1, '4': 1, '5': 5, '10': 'requestId'},
-    const {'1': 'statusCode', '3': 2, '4': 1, '5': 5, '10': 'statusCode'},
-    const {'1': 'data', '3': 3, '4': 1, '5': 9, '10': 'data'},
+  '2': [
+    {'1': 'requestId', '3': 1, '4': 1, '5': 5, '10': 'requestId'},
+    {'1': 'statusCode', '3': 2, '4': 1, '5': 5, '10': 'statusCode'},
+    {'1': 'data', '3': 3, '4': 1, '5': 9, '10': 'data'},
   ],
 };
 
 /// Descriptor for `RequestResult`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List requestResultDescriptor = $convert.base64Decode('Cg1SZXF1ZXN0UmVzdWx0EhwKCXJlcXVlc3RJZBgBIAEoBVIJcmVxdWVzdElkEh4KCnN0YXR1c0NvZGUYAiABKAVSCnN0YXR1c0NvZGUSEgoEZGF0YRgDIAEoCVIEZGF0YQ==');
+final $typed_data.Uint8List requestResultDescriptor = $convert.base64Decode(
+    'Cg1SZXF1ZXN0UmVzdWx0EhwKCXJlcXVlc3RJZBgBIAEoBVIJcmVxdWVzdElkEh4KCnN0YXR1c0'
+    'NvZGUYAiABKAVSCnN0YXR1c0NvZGUSEgoEZGF0YRgDIAEoCVIEZGF0YQ==');
+
 @$core.Deprecated('Use requestErrorDescriptor instead')
-const RequestError$json = const {
+const RequestError$json = {
   '1': 'RequestError',
-  '2': const [
-    const {'1': 'requestId', '3': 1, '4': 1, '5': 5, '10': 'requestId'},
-    const {'1': 'statusCode', '3': 2, '4': 1, '5': 5, '10': 'statusCode'},
-    const {'1': 'error', '3': 3, '4': 1, '5': 9, '10': 'error'},
+  '2': [
+    {'1': 'requestId', '3': 1, '4': 1, '5': 5, '10': 'requestId'},
+    {'1': 'statusCode', '3': 2, '4': 1, '5': 5, '10': 'statusCode'},
+    {'1': 'error', '3': 3, '4': 1, '5': 9, '10': 'error'},
   ],
 };
 
 /// Descriptor for `RequestError`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List requestErrorDescriptor = $convert.base64Decode('CgxSZXF1ZXN0RXJyb3ISHAoJcmVxdWVzdElkGAEgASgFUglyZXF1ZXN0SWQSHgoKc3RhdHVzQ29kZRgCIAEoBVIKc3RhdHVzQ29kZRIUCgVlcnJvchgDIAEoCVIFZXJyb3I=');
+final $typed_data.Uint8List requestErrorDescriptor = $convert.base64Decode(
+    'CgxSZXF1ZXN0RXJyb3ISHAoJcmVxdWVzdElkGAEgASgFUglyZXF1ZXN0SWQSHgoKc3RhdHVzQ2'
+    '9kZRgCIAEoBVIKc3RhdHVzQ29kZRIUCgVlcnJvchgDIAEoCVIFZXJyb3I=');
+
 @$core.Deprecated('Use notificationDescriptor instead')
-const Notification$json = const {
+const Notification$json = {
   '1': 'Notification',
-  '2': const [
-    const {'1': 'subscriptionId', '3': 1, '4': 1, '5': 5, '10': 'subscriptionId'},
-    const {'1': 'data', '3': 2, '4': 1, '5': 9, '10': 'data'},
+  '2': [
+    {'1': 'subscriptionId', '3': 1, '4': 1, '5': 5, '10': 'subscriptionId'},
+    {'1': 'data', '3': 2, '4': 1, '5': 9, '10': 'data'},
   ],
 };
 
 /// Descriptor for `Notification`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List notificationDescriptor = $convert.base64Decode('CgxOb3RpZmljYXRpb24SJgoOc3Vic2NyaXB0aW9uSWQYASABKAVSDnN1YnNjcmlwdGlvbklkEhIKBGRhdGEYAiABKAlSBGRhdGE=');
+final $typed_data.Uint8List notificationDescriptor = $convert.base64Decode(
+    'CgxOb3RpZmljYXRpb24SJgoOc3Vic2NyaXB0aW9uSWQYASABKAVSDnN1YnNjcmlwdGlvbklkEh'
+    'IKBGRhdGEYAiABKAlSBGRhdGE=');
+
 @$core.Deprecated('Use notificationErrorDescriptor instead')
-const NotificationError$json = const {
+const NotificationError$json = {
   '1': 'NotificationError',
-  '2': const [
-    const {'1': 'subscriptionId', '3': 1, '4': 1, '5': 5, '10': 'subscriptionId'},
-    const {'1': 'statusCode', '3': 2, '4': 1, '5': 5, '10': 'statusCode'},
-    const {'1': 'error', '3': 3, '4': 1, '5': 9, '10': 'error'},
+  '2': [
+    {'1': 'subscriptionId', '3': 1, '4': 1, '5': 5, '10': 'subscriptionId'},
+    {'1': 'statusCode', '3': 2, '4': 1, '5': 5, '10': 'statusCode'},
+    {'1': 'error', '3': 3, '4': 1, '5': 9, '10': 'error'},
   ],
 };
 
 /// Descriptor for `NotificationError`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List notificationErrorDescriptor = $convert.base64Decode('ChFOb3RpZmljYXRpb25FcnJvchImCg5zdWJzY3JpcHRpb25JZBgBIAEoBVIOc3Vic2NyaXB0aW9uSWQSHgoKc3RhdHVzQ29kZRgCIAEoBVIKc3RhdHVzQ29kZRIUCgVlcnJvchgDIAEoCVIFZXJyb3I=');
+final $typed_data.Uint8List notificationErrorDescriptor = $convert.base64Decode(
+    'ChFOb3RpZmljYXRpb25FcnJvchImCg5zdWJzY3JpcHRpb25JZBgBIAEoBVIOc3Vic2NyaXB0aW'
+    '9uSWQSHgoKc3RhdHVzQ29kZRgCIAEoBVIKc3RhdHVzQ29kZRIUCgVlcnJvchgDIAEoCVIFZXJy'
+    'b3I=');
